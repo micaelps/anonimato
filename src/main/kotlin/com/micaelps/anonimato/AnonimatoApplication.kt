7ad631d9b252a00ba067d5195a2dfa3c790ea376
micaelps <3.1416micael@gmail.com>
2021-04-11 21:34:58 -0300

    iniciando projeto

---
+++ src/main/kotlin/com/micaelps/anonimato/AnonimatoApplication.kt
@@ -0,0 +1,11 @@
+package com.micaelps.anonimato
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnonimatoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnonimatoApplication>(*args)
+}
---
+++ src/test/kotlin/com/micaelps/anonimato/AnonimatoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.micaelps.anonimato
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnonimatoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
